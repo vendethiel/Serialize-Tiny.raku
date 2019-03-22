45030a85fa09083c85fa6b4a0cb88b2ae56b0406
vendethiel <vendethiel@hotmail.fr>
2019-03-22 15:47:19 +0100

    Allow a callable for class-key

--- lib/Serialize/Tiny.pm
+++ lib/Serialize/Tiny.pm
@@ -3,19 +3,27 @@ unit module Serialize::Tiny;
 multi sub serialize(Mu:U, *%) is export {
   Nil
 }
-sub serialize-array(Mu:D @obj, Str :$class-key) {
+sub serialize-array(Mu:D @obj, :$class-key) {
   @obj.map({ serialize($_, :$class-key) })
 }
 # TODO serialize-hash
 # TODO do this via multi dispatch??
-multi sub serialize(Mu:D \obj, Str :$class-key) is export {
-  return obj if obj ~~ any(Str, Int, Bool);
+multi sub serialize(Mu:D \obj, :$class-key) is export {
+  return obj if obj ~~ Str | Int | Bool;
   return serialize-array(obj, :$class-key) if obj ~~ Array; # TODO Positional?
   my \type = obj.WHAT;
   my @attribute = type.^attributes.grep(*.has_accessor);
   my %attr = @attribute.map({
     .name.substr(2) => serialize(.get_value(obj), :$class-key)
   }).hash;
-  %attr{$class-key} = obj.^name if defined $class-key; # TODO try to fall back on attr type if Nil?
+  %attr ,= add-class-key($class-key, obj.^name) if defined $class-key;
   %attr
+}
+
+sub add-class-key($class-key, Str $class-name) {
+  do given $class-key {
+    when Str { $class-key => $class-name }
+    when Callable { $class-key($class-name) }
+    default { Empty }
+  }
 }
